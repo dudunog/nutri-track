f15fd500cf8a45a4b708c48fd8cbe4810fe98bd4
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-05-31 15:52:46 -0300

    feat: setup project

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
