ca6e49bf610ca2c2d2bc959239638eaca9e69b1c
Ermeson23 <joseenem35@gmail.com>
2025-06-10 16:07:40 -0300

    refactor: make FirstStep component generic and reusable for anothers stepScreens

--- app/user-objetives.tsx
+++ app/user-objetives.tsx
@@ -1,5 +1,5 @@
 import { FirstStep } from "@/presentation/components/first-step";
-import { useNavigation } from "expo-router";
+import { router, useNavigation } from "expo-router";
 import { useEffect } from "react";
 import { View } from "react-native";
 
@@ -10,9 +10,28 @@ export default function Home() {
     navigation.setOptions({ headerShown: false });
   }, [navigation]);
 
+  const handleSelect = (option: string) => {
+    console.log("Selecionando: ", option);
+  }
+
+  const options = [
+    { title: "Perder peso", onPress: () => handleSelect("Perder peso") },
+    { title: "Manter peso", onPress: () => handleSelect("Manter peso") },
+    {
+      title: "Ganhar massa muscular",
+      onPress: () => handleSelect("Ganhar massa muscular"),
+    },
+    { title: "Melhorar a saúde", onPress: () => handleSelect("Melhorar a saúde") },
+  ];
+
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
-      <FirstStep />
+      <FirstStep
+        title="Qual o seu objetivo?"
+        subtitle="Vamos precisar de algumas informações para ajustar o seu plano."
+        options={ options }
+        onContinue={() => router.push("/user-objetives")}
+       />
     </View>
   );
 }
